3de435039e5e6224a9101584611f6dea8ba6608a
T-Luxshan <luckybraveboys@gmail.com>
2023-08-07 22:20:11 +0530

    NavBar update

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -18,17 +18,7 @@ const NavBar = () => {
         <li className="nav-item">
           <a className="nav-link" href="#">Link</a>
         </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
+        
         <li className="nav-item">
           <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
         </li>
@@ -42,7 +32,6 @@ const NavBar = () => {
 </nav>
 
 </div>
-
      );
 }
  
