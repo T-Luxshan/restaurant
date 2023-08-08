361ebdf7819176c40d512a9331cb7ff6e2a463dc
T-Luxshan <luckybraveboys@gmail.com>
2023-08-09 00:43:23 +0530

    Navbar update 2

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,33 +5,32 @@ const NavBar = () => {
     return ( 
       <div className="navbar-container">
   <nav className="navbar navbar-expand-lg navbar-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="container-fluid foodie">
+      <a className="nav-link" href="#">FOODIE</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+    </div>
+    <div className="collapse navbar-collapse navigations" id="navbarSupportedContent">
+      <ul className="navbar-nav pages"> 
+        <li className="nav-item pages">
+          <a className="nav-link" href="#">Home</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+        <li className="nav-item pages">
+          <a className="nav-link" href="#">About</a>
         </li>
-        
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <li className="nav-item pages">
+          <a className="nav-link" href="#">Contacts</a>
+        </li>
+        <li className="nav-item pages">
+          <button className="primary-button" type="submit">Book&nbsp;Now</button>
         </li>
       </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
-  </div>
-</nav>
-
+  </nav>
 </div>
+
+
      );
 }
  
